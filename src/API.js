42b80e0028b22adb83ee2ef69f572e621937508f
Жанна Нарсия <z.narsija@superjob.ru>
2019-04-07 17:04:56 +0300

    module8-task1 19. Личный проект: серверное сияние

---
+++ src/API.js
@@ -0,0 +1,69 @@
+import {Adapter} from './adapter';
+
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+const toJSON = (response) => {
+  return response.json();
+};
+
+export const API = class {
+  constructor({endPoint, authorization}) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getPoints() {
+    return this._load({url: `points`})
+      .then(toJSON)
+      .then(Adapter.parsePoints);
+  }
+
+  updatePoints({id, data}) {
+    return this._load({
+      url: `points/${id}`,
+      method: Method.PUT,
+      body: JSON.stringify(data),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then(toJSON)
+      .then(Adapter.parsePoint);
+  }
+
+  deleteTask({id}) {
+    return this._load({url: `points/${id}`, method: Method.DELETE});
+  }
+
+  getDestinations() {
+    return this._load({url: `destinations`})
+      .then(toJSON);
+  }
+
+  getOffers() {
+    return this._load({url: `offers`})
+      .then(toJSON);
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        console.error(`fetch error: ${err}`);
+        throw err;
+      });
+  }
+};
--- src/edit-point.js
+++ src/edit-point.js
@@ -3,64 +3,80 @@ import flatpickr from 'flatpickr';
 import moment from 'moment';
 import {renderEvents, events} from './render-events';
 import {renderOffers} from './render-offers';
+import {destinations, offers} from './main';
 
 export class EditPoint extends Component {
   constructor(data) {
     super();
-    this._icon = data.icon;
-    this._activity = data.activity;
+    this._id = data.id;
+    this._title = data.title;
+    this._icon = events[data.title].icon;
+    this._activity = events[data.title].activity;
     this._city = data.city;
-    this._picture = data.picture;
-    this._descriptions = data.descriptions;
+    this._pictures = data.pictures;
+    this._description = data.description;
     this._date = data.date;
     this._time = data.time;
     this._price = data.price;
     this._offers = data.offers;
     this._renderEvents = renderEvents;
     this._renderOffers = renderOffers;
-
-    this._isDeleted = data.isDeleted;
-
     this._onSubmit = null;
     this._onDelete = null;
     this._onSubmitButtonClick = this._onSubmitButtonClick.bind(this);
     this._onDeleteButtonClick = this._onDeleteButtonClick.bind(this);
   }
 
+  renderOptions(destinationsListItem) {
+    const names = destinationsListItem.map((item) => item.name);
+    const optionsList = names.map((name) => {
+      return `<option value="${name}"></option>`;
+    });
+    return optionsList.join(``);
+  }
+
   update(data) {
-    this._icon = data.icon;
     this._activity = data.activity;
     this._city = data.city;
-    this._time = data.time;
-    this._price = data.price;
+    this._icon = data.icon;
     this._offers = data.offers;
+    this._price = data.price;
+    this._time = data.time;
+    this._title = data.title;
+  }
+
+  updateDesription(pictures, description) {
+    this._pictures = pictures;
+    this._description = description;
+  }
+
+  updateOffers(newOffers) {
+    this._offers = newOffers;
   }
 
   static createMapper(target) {
     return {
-      icon: (value) => (target.icon = events[value].icon),
-      destination: (value) => (target.city = value),
-      travelWay: (value) => (target.activity = value),
-      dateStart: (value) => (target.time.startTime = value),
       dateEnd: (value) => (target.time.endTime = value),
+      dateStart: (value) => (target.time.startTime = value),
+      destination: (value) => (target.city = value),
       price: (value) => (target.price = value),
-      offer: (value) => target.offers.add(value),
+      travelWay: (value) => (target.activity = value),
     };
   }
 
-  _processForm(formData) {
+  _processForm(formData, offersData) {
     const entry = {
-      icon: ``,
+      city: ``,
+      destination: ``,
+      offers: [],
+      price: ``,
+      time: new Date(),
       title: ``,
       travelWay: ``,
-      destination: ``,
-      city: ``,
-      time: new Date(),
-      price: ``,
-      offers: new Set(),
-      activity: ``,
-      isDeleted: false,
     };
+    offersData.forEach((offer) => {
+      offer.accepted = false;
+    });
 
     const editPointMapper = EditPoint.createMapper(entry);
     for (const pair of formData.entries()) {
@@ -69,17 +85,27 @@ export class EditPoint extends Component {
         editPointMapper[property](value);
       }
       if (property === `travelWay`) {
-        entry.icon = events[value].icon;
         entry.activity = events[value].activity;
+        entry.icon = events[value].icon;
+        entry.title = value;
+      }
+      if (property === `offer`) {
+        offersData.forEach((offer) => {
+          if (offer.title === value) {
+            offer.accepted = true;
+          }
+        });
       }
     }
+    entry.offers = offersData;
     return entry;
   }
 
   _onSubmitButtonClick(evt) {
     evt.preventDefault();
+    evt.target.innerText = `Saving...`;
     const formData = new FormData(this._element.querySelector(`.point__form`));
-    const newData = this._processForm(formData);
+    const newData = this._processForm(formData, this._offers);
     if (typeof this._onSubmit === `function`) {
       this._onSubmit(newData);
     }
@@ -114,10 +140,7 @@ export class EditPoint extends Component {
                   name="destination"
               />
               <datalist id="destination-select">
-                <option value="airport"></option>
-                <option value="Geneva"></option>
-                <option value="Chamonix"></option>
-                <option value="hotel"></option>
+                ${this.renderOptions(destinations)}
               </datalist>
             </div>
       
@@ -168,10 +191,10 @@ export class EditPoint extends Component {
             <section class="point__destination">
               <h3 class="point__details-title">Destination</h3>
               <p class="point__destination-text">
-                ${this._descriptions}
+                ${this._description}
               </p>
               <div class="point__destination-images">
-                <img src="${this._picture}" alt="picture from place" class="point__destination-image">
+              ${this._pictures.map((picture) => (`<img src="${picture.src}" alt="${picture.description}" class="point__destination-image">`))}
               </div>
             </section>
             
@@ -193,8 +216,23 @@ export class EditPoint extends Component {
 
   _onDeleteButtonClick() {
     if (typeof this._onDelete === `function`) {
-      this._onDelete();
+      this._onDelete({id: this._id});
+      this._element.querySelector(`.point__button--delete`).innerText = `Deleting...`;
     }
+  }
+
+  shake() {
+    const ANIMATION_TIMEOUT = 600;
+    this._element.style.border = `1px solid red`;
+    this._element.style.animation = `shake ${ANIMATION_TIMEOUT / 1000}s`;
+    this._element.querySelector(`.point__button--save`).innerText = `Save`;
+    this._element.querySelector(`.point__button--delete`).innerText = `Delete`;
+
+    setTimeout(() => {
+      this._element.style.animation = ``;
+      this._element.style.border = `none`;
+    }, ANIMATION_TIMEOUT);
+
   }
 
   bind() {
@@ -206,35 +244,76 @@ export class EditPoint extends Component {
 
     const startInputTimeElement = this._element.querySelector(`input[name='dateStart']`);
     const endInputTimeElement = this._element.querySelector(`input[name='dateEnd']`);
+
     flatpickr(startInputTimeElement, {
       'defaultDate': this._time.startTime,
       'enableTime': true,
       'time_24hr': true,
-      'noCalendar': true,
       'altInput': true,
       'altFormat': `H:i`,
-      'dateFormat': `H:i`
+      'dateFormat': `Z`
     });
 
     flatpickr(endInputTimeElement, {
       'defaultDate': this._time.endTime,
       'enableTime': true,
       'time_24hr': true,
-      'noCalendar': true,
       'altInput': true,
       'altFormat': `H:i`,
-      'dateFormat': `H:i`
+      'dateFormat': `Z`
     });
 
     const travelInputsCollection = this._element.querySelectorAll(`.travel-way__select-input`);
-    travelInputsCollection.forEach(function (travelInput) {
-      travelInput.addEventListener(`click`, function () {
+    const offersWrapElement = this._element.querySelector(`.point__offers-wrap`);
+
+    const renameObjectField = (oldKey, newKey, object) => {
+      if (oldKey !== newKey) {
+        Object.defineProperty(object, newKey, Object.getOwnPropertyDescriptor(object, oldKey));
+        delete object[oldKey];
+        object.accepted = ``;
+      }
+    };
+
+    let newOffers = [];
+
+    travelInputsCollection.forEach((travelInput) => {
+      travelInput.onclick = () => {
         travelInput.setAttribute(`checked`, true);
-        document.querySelector(`.point__destination-label`).innerText = events[travelInput.value].activity;
         document.querySelector(`.travel-way__label`).innerText = events[travelInput.value].icon;
+        document.querySelector(`.point__destination-label`).innerText = events[travelInput.value].activity;
         document.querySelector(`.travel-way__toggle`).checked = false;
-      });
+        offers.some(function (offer) {
+          if (travelInput.value === offer.type) {
+            newOffers = offer.offers;
+            newOffers.forEach(function (newOffer) {
+              if (newOffer !== null) {
+                renameObjectField(`name`, `title`, newOffer);
+              }
+            });
+          }
+          offersWrapElement.innerHTML = renderOffers(newOffers);
+        });
+        this.updateOffers(newOffers);
+      };
     });
+
+    const destinationInputElement = this._element.querySelector(`.point__destination-input`);
+    const destinationTextElement = this._element.querySelector(`.point__destination-text`);
+    const destinationImageElements = this._element.querySelector(`.point__destination-images`);
+
+    destinationInputElement.onchange = () => {
+      let description = ``;
+      let photos = [];
+      destinations.some(function (destination) {
+        if (destinationInputElement.value === destination.name) {
+          destinationTextElement.innerText = destination.description;
+          description = destination.description;
+          photos = destination.pictures;
+          destinationImageElements.innerHTML = destination.pictures.map((picture) => (`<img src="${picture.src}" alt="${picture.description}" class="point__destination-image">`)).join(``);
+        }
+      });
+      this.updateDesription(photos, description);
+    };
   }
 
   unbind() {
